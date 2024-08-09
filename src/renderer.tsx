277febbebccec2b9f3f97b695ca3824f6a6ab9e0
Liam Tsang <lt1800@nyu.edu>
2024-08-08 22:50:14 -0400

    mobile 2

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -7,7 +7,7 @@ export const renderer = jsxRenderer(({ children, title }) => {
         <meta charset='UTF-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         <link href='/static/style.css' rel='stylesheet' />
-        <title>Your Page Title</title>
+        <title>Dailies / dles</title>
         <link
           rel='stylesheet'
           href='https://unpkg.com/pollen-css@4.0.0/pollen.css'
